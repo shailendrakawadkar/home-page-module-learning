f6e7bf340373c15f31d727e3f4635eb2bb5b9e35
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2021-01-05 10:03:52 +0530

    added component

--- src/app/nav-side/nav-side.module.ts
+++ src/app/nav-side/nav-side.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, SidebarComponent],
   imports: [
     CommonModule
+  ],
+  exports : [
+    NavbarComponent,
+    SidebarComponent
   ]
 })
 export class NavSideModule { }
