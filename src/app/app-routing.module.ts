a444cf8a88c1250d53cbe5da0bf7647d5f930d0e
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2021-01-05 22:25:00 +0530

    Completed
    I learned module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ComtactMeComponent } from './comtact-me/comtact-me.component';
 import { HomeComponent } from './home/home.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
-   {path: 'home', component: HomeComponent},
+   {path: 'whoAmI', component: HomeComponent},
+   {path: 'skills', component: SkillsComponent},
+   {path: 'profiles', component: ProfilesComponent},
+   {path: 'contact-me', component: ComtactMeComponent},
+   {path: '', component: HomeComponent},
 ];
 
 @NgModule({
